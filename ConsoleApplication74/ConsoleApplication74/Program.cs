af4316775af90c260346459508bc1fc5d7a1914a
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-11 12:47:04 +0100

    Add files via upload

---
+++ ConsoleApplication74/ConsoleApplication74/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liczby = new int[4];
+
+            int[] liczby2 = { 1,2,7,6};
+            //for(int i=0;i<liczby.Length; i++)
+            //{
+            //    Console.WriteLine(liczby2[i]);
+            //}
+            //Console.WriteLine("foreach");
+            //foreach(var element in liczby2)
+            //{
+            //    Console.WriteLine(element);
+            //}
+            //Console.WriteLine(liczby2[liczby2.Length-1]);
+            Array.Sort(liczby2);
+            
+            //Osoba[] osoby = new Osoba[2];
+            //Osoba[] osoby2 = { new Osoba("Jan", 50), new Osoba("Anna", 25), new Osoba("Krzysztof", 40) };
+            //Osoba[] osoby3 = osoby2;
+            //osoby2[1] = new Osoba("Zofia", 60);
+            //Array.Resize<Osoba>(ref osoby2, 6);
+            //osoby2[2] = new Osoba("Tomasz", 12);
+
+            Stack<Osoba> osoby5 = new Stack<Osoba>();
+            
+        }
+    }
+
+    class Osoba
+    {
+        int wiek;
+        string imie;
+
+        public Osoba(string imie, int wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+    }
+}
---
+++ ConsoleApplication76/ConsoleApplication76/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> wyrazy = new Stack<string>();
+            //wyrazy.Push("sddssd");
+            ////wyrazy.Push(675666);
+            ////wyrazy[0];
+            //wyrazy.Push("334");
+            //wyrazy.Push("");
+            //wyrazy.Push("sddssd");
+            //wyrazy.Push("awe");
+            //wyrazy.Push("654");
+            //Console.WriteLine(wyrazy.Contains("rttt"));
+            //foreach(var element in wyrazy)
+            //{
+            //    Console.WriteLine(element);
+            //}
+            //Console.WriteLine(wyrazy.Peek());
+            //for (int i = 0; i < wyrazy.Count; i++)
+            //{
+            //    Console.WriteLine(wyrazy[i]);
+            //}
+            Osoba o1 = new Osoba("Jan", 23);
+            Osoba o2 = new Osoba("Anna", 20);
+            Osoba o3 = new Osoba("Krzysztof", 44);
+            Stack<Osoba> osoby = new Stack<Osoba>();
+            osoby.Push(o1);
+            osoby.Push(o2);
+            osoby.Push(o3);
+            Console.WriteLine(osoby.Contains(o1));
+            var osoby2 = osoby.Reverse().ToList<Osoba>();
+
+
+
+        }
+    }
+
+    class Osoba
+    {
+        int wiek;
+        string imie;
+
+        public Osoba(string imie, int wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+
+        public Osoba() { }
+    }
+
+    class Student:Osoba
+    {
+        public Student() 
+        { }
+    }
+}
