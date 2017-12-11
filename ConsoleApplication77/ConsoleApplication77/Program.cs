6486c543a4a81df3cfcaf90d23c7d3e377489c59
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-11 12:48:26 +0100

    Add files via upload

---
+++ ConsoleApplication77/ConsoleApplication77/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ConsoleApplication77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liczby = new int[4];
+            //Console.WriteLine(liczby[1]);
+
+            int[] liczby2 = { 4, 2, 7, 8, -2 };
+            //int[] liczby3 = liczby2;
+            //liczby2[1] = 20;
+            //byte zmien = 3;
+            //liczby[1] = zmien;
+            //Array.Resize<int>(ref liczby2, 3);
+            for(int i=0;i<liczby2.Length;i++)
+            {
+                Console.WriteLine(liczby2[i]);
+            }
+            Console.WriteLine("foreach");
+            foreach(var element in liczby2)
+            {
+                Console.WriteLine(element);
+            }
+            //Array.Sort(liczby2);
+            
+            Osoba[] osoby1 = new Osoba[3];
+            Osoba[] osoby2 = { new Osoba("Jan", 30), new Osoba("Anna", 12) };
+
+        }
+    }
+
+    class Osoba
+    {
+        int wiek;
+        string imie;
+
+        public Osoba(string imie, int wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+    }
+}
---
+++ ConsoleApplication78/ConsoleApplication78/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> znaki = new Stack<char>();
+            znaki.Push('4');
+            znaki.Push('t');
+            znaki.Push('R');
+            znaki.Push('4');
+            
+            Console.WriteLine(znaki.Contains('R'));
+            //foreach(var element in znaki)
+            //{
+            //    Console.WriteLine(element);
+            //}
+
+            Osoba o1 = new Osoba("Jan", 23);
+            Stack<Osoba> osoby = new Stack<Osoba>();
+            osoby.Push(o1);
+
+            osoby.Push(new Osoba("Anna", 30));
+            osoby.Push(o1);
+            osoby.Push(new Student("Zofia", 19));
+            Console.WriteLine(osoby.Contains(o1));
+            //foreach(var osoba in osoby)
+            //{
+            //    Console.WriteLine(osoba);
+            //}
+            //Console.WriteLine(osoby.Pop());
+        }
+    }
+
+    class Osoba
+    {
+        int wiek;
+        string imie;
+
+        public Osoba(string imie, int wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+
+        public override string ToString()
+        {
+            return imie +", "+wiek;
+        }
+    }
+
+    class Student : Osoba
+    {
+
+        public Student(string imie, int wiek)
+            :base(imie, wiek)
+        {
+            
+        }
+
+    }
+}
